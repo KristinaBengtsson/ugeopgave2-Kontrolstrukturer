0a8d58c061378fb13581e172b5a01cb40d7cf8c6
KristinaBengtsson <cph-kd104@stud.ek.dk>
2026-02-18 09:32:07 +0100

    Project startet

---
+++ src/Opgave2.java
@@ -0,0 +1,10 @@
+public class Opgave2 {
+    double weight = 84.4;
+    double height = 181.0;
+
+    void main(){
+        double bmi = weight / (height * height);
+        System.out.println(bmi);
+
+    }
+}
